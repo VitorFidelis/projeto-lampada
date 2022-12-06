500a2bc754baa211b4ad659675b28b1c48eb9e74
Vitor Fidelis <vitorf5527@gmail.com>
2022-12-05 22:54:31 -0300

    primeiro commit

---
+++ script/script.js
@@ -0,0 +1,15 @@
+var btn_ON = document.getElementById('btn_ON');
+var btn_OFF = document.getElementById('btn_OFF');
+var lamp = document.getElementById('lamp');
+
+function Ligar(){
+    if(btn_ON.Value = 'ON'){
+        lamp.src='./imgs/lampada-On.png';
+    }
+}
+
+function Desligar(){
+    if(btn_OFF.Value ='OFF'){
+        lamp.src='./imgs/lampada-Off.jpg';
+    }
+}
